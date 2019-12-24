e2152e1b3f812e25401c6b3856b40e3cc596ff2a
taha uygun <tahauygun@gmail.com>
2019-12-23 21:58:10 -0800

    navlink work

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Box, Text,Heading,Image } from "gestalt";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-)
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="around"
+    height={70}
+    color="midnight"
+    padding={1}
+    shape={"roundedBottom"}
+  >
+    <NavLink to="/signin">
+      <Text size="xl" color="white">
+        Sign In
+      </Text>
+    </NavLink>
+    <NavLink to="/">
+      <Image src"./icons/logo.svg" alt="BrewHaha Logo" naturalHeight={1} naturalWidth={1}/>
+      <Heading size="xs" color="orange">
+        BrewHaha
+      </Heading>
+    </NavLink>
+    <NavLink to="/signup">
+      <Text size="xl" color="white">
+        Sign Up
+      </Text>
+    </NavLink>
+  </Box>
+);
 
 export default Navbar;
