1cb2f71a458bc4b45e120dfbc58aea9e7b2de026
taha uygun <tahauygun@gmail.com>
2019-12-23 21:45:01 -0800

    adding gestalt

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import './index.css';
+import "gestalt/dist/gestalt.css";
 
 import Signin from './components/Signin';
 import Signup from './components/Signup';
@@ -15,7 +15,7 @@ import registerServiceWorker from './registerServiceWorker';
 const Root = () => (
   <Router>
     <Switch>
-      <Route component={App} path="/"/>
+      <Route component={App} exact path="/"/>
       <Route component={Signin} path="/signin"/>
       <Route component={Signup} path="/signup"/>
       <Route component={Checkout} path="/checkout"/>
