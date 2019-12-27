7b3062eebe88ee4ac21e3693ed841f7a2068276a
taha uygun <tahauygun@gmail.com>
2019-12-26 23:00:09 -0800

    responsiveness

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import {Container,Box, Heading,Card,Image, Text} from "gestalt";
+import React, { Component } from "react";
+import "./App.css";
+import { Container, Box, Heading, Card, Image, Text } from "gestalt";
 import Strapi from "strapi-sdk-javascript/build/main";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-
-const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
 class App extends Component {
-  state={
+  state = {
     brands: []
-  }
-  async componentDidMount(){
-    try{
-      const response = await strapi.request('POST', '/graphql',{
-        data:{
+  };
+  async componentDidMount() {
+    try {
+      const response = await strapi.request("POST", "/graphql", {
+        data: {
           query: `query{
           brands {
             _id
@@ -28,37 +27,59 @@ class App extends Component {
         }`
         }
       });
-      this.setState({brands: response.data.brands})
-    } catch(err){
-      console.log(err)
+      this.setState({ brands: response.data.brands });
+    } catch (err) {
+      console.log(err);
     }
-
   }
   render() {
-    const {brands } = this.state;
+    const { brands } = this.state;
     return (
       <Container>
-        <Box
-          display='flex'
-          justifyContent='center'
-          marginBottom={2}
-        >
-          <Heading color='midnight' size='md'>
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          <Heading color="midnight" size="md">
             Brew Brands
           </Heading>
         </Box>
-        <Box wrap display='flex' justifyContent='around'>
-          {brands.map(brand=>(
-            <Box margin={2} width={200} key={brand._id}>
-              <Card image={
-                <Box height={200} width={200}>
-                  <Image src={`${apiUrl}${brand.image.url}`} alt='Brand' naturalWidth={1} naturalHeight={1}/>
-                </Box>
-              }>
-                <Box display='flex' alignItems='center' justifyContent='center' direction='column'>
-                  <Text bold size='xl'>{brand.name}</Text>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: "#d6c8ec"
+            }
+          }}
+          shape="rounded"
+          wrap
+          display="flex"
+          justifyContent="around"
+        >
+          {brands.map(brand => (
+            <Box paddingY={4} margin={2} width={200} key={brand._id}>
+              <Card
+                image={
+                  <Box height={200} width={200}>
+                    <Image
+                      fit="cover"
+                      src={`${apiUrl}${brand.image.url}`}
+                      alt="Brand"
+                      naturalWidth={1}
+                      naturalHeight={1}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">
+                    {brand.name}
+                  </Text>
                   <Text>{brand.description}</Text>
-                  <Text bold size='xl'><Link to={`/${brand._id}`}>See Brews</Link></Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brand._id}`}>See Brews</Link>
+                  </Text>
                 </Box>
               </Card>
             </Box>
