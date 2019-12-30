d86706bbb7c58b081715151e3fe03c7bb79246eb
taha uygun <tahauygun@gmail.com>
2019-12-29 22:55:58 -0800

    adding persist cart to localstorage

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,15 +1,8 @@
 import React from "react";
 import Strapi from "strapi-sdk-javascript/build/main";
-import {
-  Box,
-  Heading,
-  Text,
-  Image,
-  Card,
-  Button,
-  Mask,
-  IconButton
-} from "gestalt";
+// prettier-ignore
+import { Box, Heading, Text, Image, Card, Button, Mask, IconButton } from "gestalt";
+import { calculatePrice, setCart, getCart } from "../utils";
 import { Link } from "react-router-dom";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
@@ -44,7 +37,8 @@ class Brews extends React.Component {
       });
       this.setState({
         brews: response.data.brand.brews,
-        brand: response.data.brand.name
+        brand: response.data.brand.name,
+        cartItems: getCart()
       });
     } catch (err) {
       console.error(err);
@@ -61,12 +55,19 @@ class Brews extends React.Component {
         ...brew,
         quantity: 1
       });
-      this.setState({ cartItems: updatedItems });
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
     } else {
       const updatedItems = [...this.state.cartItems];
       updatedItems[alreadyInCart].quantity += 1;
-      this.setState({ cartItems: updatedItems });
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
     }
+  };
+
+  deleteItemFromCart = itemToDeleteId => {
+    const filteredItems = this.state.cartItems.filter(
+      item => item._id !== itemToDeleteId
+    );
+    this.setState({ cartItems: filteredItems }, () => setCart(filteredItems));
   };
 
   render() {
@@ -157,7 +158,7 @@ class Brews extends React.Component {
               padding={2}
             >
               {/* User Cart Heading */}
-              <Heading align="center" size="md">
+              <Heading align="center" size="sm">
                 Your Cart
               </Heading>
               <Text color="gray" italic>
@@ -176,6 +177,7 @@ class Brews extends React.Component {
                     icon="cancel"
                     size="sm"
                     iconColor="red"
+                    onClick={() => this.deleteItemFromCart(item._id)}
                   />
                 </Box>
               ))}
@@ -191,7 +193,7 @@ class Brews extends React.Component {
                     <Text color="red">Please select some items</Text>
                   )}
                 </Box>
-                <Text size="lg">Total: $3.99</Text>
+                <Text size="lg">Total: {calculatePrice(cartItems)}</Text>
                 <Text>
                   <Link to="/checkout">Checkout</Link>
                 </Text>
